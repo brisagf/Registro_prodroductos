f0e95678a5f1e0104bf68b5b5c7e26249c5590db
brisagf <151808091+brisagf@users.noreply.github.com>
2023-11-22 22:38:10 -0600

    Primera Version Producto Controlador

---
+++ Controlador.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author brisa
+ */
+public class Controlador {
+    Producto pro=new Producto();
+    
+    public static void Registra(String nombre, String marca,String tipo, String contenido,int cantidad,int precio){
+        
+    }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    
+    
+    
+    
+    
+    
+}
---
+++ Producto.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author brisa
+ */
+public class Producto {
+    
+    String nombre;
+    int precio;
+    String contenido;
+    String marca;
+    String tipo;
+    int cantidad;
+
+    
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+ public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+ 
+    public int getPrecio() {
+        return precio;
+    }
+
+   public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+     public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+   
+    
+    
+            
+    
+}
